78e59817ddebae4d76c633b4b98866986d1c27ab
srepking <shanerepking@gmail.com>
2020-10-26 21:52:32 -0700

    Inline Category Edits from Post Admin is Working

--- blogging/admin.py
+++ blogging/admin.py
@@ -3,5 +3,22 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine,
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine
+    ]
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
