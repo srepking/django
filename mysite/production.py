15da3ad867b1c266dc4fd75fd1cbd729aca39cb4
Ubuntu <py230admin@Python230-ubuntu0001009.qseowevrukjezjmisq4n1jj0ke.dx.internal.cloudapp.net>
2020-11-11 04:42:53 +0000

    Gunicorn Not autostarting server-Please help

--- mysite/production.py
+++ mysite/production.py
@@ -2,14 +2,14 @@ import os
 
 import dj_database_url
 
-from .settings import *
+from mysite.settings import *
 
 
 DATABASES = {
     'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
     }
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
